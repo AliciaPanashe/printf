8532d0dea6de8212ba8eddc287ebc3f19b8e1fec
AliciaPanashe <aliciapanashe@gmail.com>
2023-03-28 03:23:01 -0700

    main header

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
-
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
+#define UNUSED(x) void(x)
 #define BUFF_SIZE 1024
 
 /* FLAGS */
